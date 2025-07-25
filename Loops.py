0a01c403398858813279e96d064f7b4ff36d0865
Abdulrafay Jahangir <jabdulrafay69@gmail.com>
2025-07-25 11:11:10 +0500

    Create Loops.py

---
+++ Loops.py
@@ -0,0 +1,43 @@
+#while loop
+count =1
+while count<=5:
+    print("Hello World")
+    count+=1
+
+#numbers in while loop
+i=1
+while i<=5:
+    print(i)
+    i+=1
+
+#practice question (Table)
+n=int(input("Enter a number :"))
+i=1
+while i<=10:
+    print(n*i)
+    i+=1
+
+#practice question 2
+num=[1,4,9,16,25,36,49,64,81,100]
+i=0
+while i<=9:
+    print(num[i])
+    i+=1
+
+#for loops
+nums=[1,2,3,4,5]
+
+for val in nums:
+    print(val)
+
+tup=(1,2,3,4,5)
+for vale in tup:
+    print(vale)
+
+
+#printing characters in a string with for loop
+name= "Abdulrafay"
+
+for chr in name:
+    print(chr)
+
